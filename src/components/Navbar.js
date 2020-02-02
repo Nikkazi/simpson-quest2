1de4fc8d890b7a737bc07b8743280cafbcd56c9d
Nikkazi <nickkazi@hotmail.com>
2020-02-02 11:53:17 +0000

    simpson  first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+            <a className="Nav-link" href="https://en.wikipedia.org/wiki/The_Simpsons">
+                simpsonWiki</a>
+        </nav>
+    );
+}
+export default Navbar;
