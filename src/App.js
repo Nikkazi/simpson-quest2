6c9879aaa0b6abf67e8da9a98fc6daacf1826630
Nikkazi <nickkazi@hotmail.com>
2020-02-03 19:54:49 +0000

    full codes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-
+import QuoteCard from './components/QuoteCard';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <QuoteCard
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+        quote="Shoplifting is a victimless crime, like punching someone in the dark."
+        character="Nelson Muntz"
+      />
+
+      <QuoteCard
+        quote="Oh Yeah!"
+        image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FDuffman.png?1497567511709"
+        character="Duffman"
+
+      />
+
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,7 @@ import './Navbar.css';
 function Navbar() {
     return (
         <nav className="Navbar">
-            <a className="Nav-link" href="https://en.wikipedia.org/wiki/The_Simpsons">
-                simpsonWiki</a>
+            <a className="Nav-link" href="https://en.wikipedia.org/wiki/The_Simpsons">simpsonWiki</a>
         </nav>
     );
 }
